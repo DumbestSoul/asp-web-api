272990fd0c5c2dafc93f969be3968e9ca80df9ca
Rajat Kishore Acharya <rajat.getmeonfb@gmail.com>
2024-04-08 02:39:33 +0530

    Finally all file uploaded

---
+++ Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace EmpAPI.Models
+{
+	public class Employee
+	{
+		public int Id { get; set; }
+
+		public string? Name { get; set; }
+        public string Postition { get; set; }
+		
+		public double Salary { get; set; }
+		public DateTime JoinDate { get; set; }
+    }
+}
